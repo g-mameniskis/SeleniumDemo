56608c80499a69268d73564069854a57b44b084b
gmamenis <gmamenis@iu.edu>
2020-05-29 09:58:59 -0400

    Headless Mode achieved

--- SeleniumDemo/src/com/company/tests/MyFirstTest.java
+++ SeleniumDemo/src/com/company/tests/MyFirstTest.java
@@ -2,6 +2,7 @@ package com.company.tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 // This project is a demo from Udemy Course 'Selenium Webdriver Java: Learn from Scratch'
 // by instructor Kiran Signh
@@ -16,8 +17,13 @@ public class MyFirstTest {
             // value is the location of the chromedriver
         System.setProperty("webdriver.chrome.driver",
                 "/Users/gmamenis/MyWork/PerScholas/dev/SeleniumDemo/SeleniumDemo/src/drivers/chromedriver");
+        // 6. Headless Mode Testing (testing without browser UI or GUI)
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setHeadless(true);
         // 2. new driver instance
-        WebDriver driver = new ChromeDriver();
+            // initially, you don't need to pass any arguments to driver
+            // however, you must pass chromeoptions in order to achieve headless mode
+        WebDriver driver = new ChromeDriver(chromeOptions);
         // 3. this is the app we will test
             // this creates an instance of the webpage below in a google chrome browser
         driver.get("https://opensource-demo.orangehrmlive.com/");
