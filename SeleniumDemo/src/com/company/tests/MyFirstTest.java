59ddaa33d8a62c0bd590f398ac7ccc03213d21f9
gmamenis <gmamenis@iu.edu>
2020-05-29 09:48:24 -0400

    successfully maximized browser instance and printed title of webpage to console

--- SeleniumDemo/src/com/company/tests/MyFirstTest.java
+++ SeleniumDemo/src/com/company/tests/MyFirstTest.java
@@ -13,12 +13,18 @@ public class MyFirstTest {
     public static void main(String[] args) {
 
         // 1. set properties of chrome driver (key and value)
+            // value is the location of the chromedriver
         System.setProperty("webdriver.chrome.driver",
                 "/Users/gmamenis/MyWork/PerScholas/dev/SeleniumDemo/SeleniumDemo/src/drivers/chromedriver");
         // 2. new driver instance
         WebDriver driver = new ChromeDriver();
         // 3. this is the app we will test
+            // this creates an instance of the webpage below in a google chrome browser
         driver.get("https://opensource-demo.orangehrmlive.com/");
+        // 4. this maximizes the browser window
+        driver.manage().window().maximize();
+        // 5. this retrieves the title of the web page
+        System.out.println("Web Page Title: " + driver.getTitle());
 
     }
 }
